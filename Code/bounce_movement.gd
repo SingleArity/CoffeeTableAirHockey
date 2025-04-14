78d0bf99514bf62517c5403a5f5eeee233a6c837
TJ <tmelanson17@gmail.com>
2025-04-13 22:16:34 -0700

    Put puck in the front

--- Code/bounce_movement.gd
+++ Code/bounce_movement.gd
@@ -1,18 +1,19 @@
-extends RigidBody2D
+extends Node2D
 
 var velocity = Vector2.ZERO
 var speed = 200
 var bounds = Rect2(Vector2(0, 0), Vector2(800, 600)) # Example edge limits (adjust as needed)
-var rigid_body
+var rigid_body 
 
 func _ready():
+	rigid_body = $RigidPhysics
 	# Initialize random direction
 	var angle = PI # randf_range(0, 2 * PI)
 	velocity = Vector2(cos(angle), sin(angle)) * speed
-	linear_velocity = velocity
+	rigid_body.linear_velocity = velocity
 
 func _physics_process(delta):
 	# Move and collide with walls if needed
-	var collision_info = move_and_collide(velocity * delta)
+	var collision_info = rigid_body.move_and_collide(velocity * delta)
 	if collision_info:
 		velocity = velocity.bounce(collision_info.get_normal())
